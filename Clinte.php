01064cb2004bbb4d79febf413c162c19881ffb35
reza farazi <rezafarazi1378@gmail.com>
2023-02-14 14:29:43 +0330

    OK

--- Clinte.php
+++ Clinte.php
@@ -1,11 +1,19 @@
 <?php
 
-
-$SOCKET=socket_create(AF_INET,SOCK_DGRAM,0);
-socket_connect($SOCKET,"localhost:63342","8080");
-socket_write($SOCKET,'Salam',5);
-echo "OK";
-
-
+$host    = "127.0.0.1";
+$port    = 25006;
+$message = "Hello Server";
+echo "Message To server :".$message;
+// create socket
+$socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
+// connect to server
+$result = socket_connect($socket, $host, $port) or die("Could not connect to server\n");  
+// send string to server
+socket_write($socket, $message, strlen($message)) or die("Could not send data to server\n");
+// get server response
+$result = socket_read ($socket, 1024) or die("Could not read server response\n");
+echo "Reply From Server  :".strrev($result);
+// close socket
+socket_close($socket);
 
 ?>
--- UTP.php
+++ UTP.php
@@ -1,20 +1,31 @@
 <?php
-
+// set some variables
+$host = "127.0.0.1";
+$port = 25006;
+// don't timeout!
 set_time_limit(0);
-$SOCKET=socket_create(AF_INET,SOCK_DGRAM,0);
-$Result=socket_bind($SOCKET,'localhost:63342','8080');
-$Result=socket_listen($SOCKET,3);
+// create socket
+$socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
+// bind socket to port
+$result = socket_bind($socket, $host, $port) or die("Could not bind to socket\n");
+// start listening for connections
+$result = socket_listen($socket, 3) or die("Could not set up socket listener\n");
 
-echo "OK";
+// accept incoming connections
+// spawn another socket to handle communication
+$spawn = socket_accept($socket) or die("Could not accept incoming connection\n");
+// read client input
+$input = socket_read($spawn, 1024) or die("Could not read input\n");
+// clean up input string
+$input = trim($input);
+echo "Client Message : ".$input;
+// reverse client input and send back
+$output = strrev("Salam") . "\n";
+socket_write($spawn, $output, strlen ($output)) or die("Could not write output\n");
+// close sockets
+socket_close($spawn);
 
-do
-{
-    $accept = socket_accept($SOCKET);
-    $msg = socket_read($SOCKET, 1024);
-    echo $msg;
-}
-while(true);
-
+socket_close($socket);
 
 
 ?>
